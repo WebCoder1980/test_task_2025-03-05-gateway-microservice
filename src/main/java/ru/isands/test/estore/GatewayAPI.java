48f74e218cb8151e88bd57a8d07f71552230b9d1
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2025-03-08 13:28:15 +0700

    Change @SpringBootConfiguration to @Configuration on GatewayAPI.java.

--- src/main/java/ru/isands/test/estore/GatewayAPI.java
+++ src/main/java/ru/isands/test/estore/GatewayAPI.java
@@ -1,12 +1,12 @@
 package ru.isands.test.estore;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class GatewayAPI {
     @Value("${EMPLOYEE_MICROSERVICE_HOST:localhost}")
     private String employeeMicroserviceHost;
